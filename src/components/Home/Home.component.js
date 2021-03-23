fa824be7d9c20631ff38e00e1accbe1ae135aa5b
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-22 17:51:31 -0700

    using background for form

--- src/components/Home/Home.component.js
+++ src/components/Home/Home.component.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import InputField from '../InputField/InputField.component';
-import './Home.styles.css';
+import React from 'react'
+import './Home.styles.css'
 
 function Home() {
     return (
         <div className="home">
-            <div className="home__row1">
-                <InputField label="FIRST NAME" />
-                <InputField label="LAST NAME" />
-            </div>
-            <div className="home__row2">
-                <InputField label="EMAIL" />
-                <InputField label="OGRANIZATION" />
-            </div>
-
+            testing
         </div>
     )
 }
