5eef038b3b5402352f1aeaf47ae0f9a3ebde86d3
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-03-23 13:28:52 -0700

    using grid from material ui

--- src/components/Home/Home.component.js
+++ src/components/Home/Home.component.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import { Grid } from '@material-ui/core';
+import React, { useState } from 'react'
 import './Home.styles.css'
 
+const initialValues = {
+    id: 0,
+    fullname: '',
+    email: '',
+    mobile: '',
+    city: '',
+    gender: 'male',
+    departmentId: '',
+    hireDate: new Date(),
+    isPermanent: false,
+}
+
 function Home() {
+    const [values, setValues] = useState(initialValues);
     return (
         <div className="home">
-            testing
+            <form>
+                <Grid container>
+                    <Grid item></Grid>
+                    <Grid item></Grid>
+                </Grid>
+            </form>
         </div>
     )
 }
